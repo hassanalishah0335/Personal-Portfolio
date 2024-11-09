8a7d9ac274d51a6ed5d46aaef1d06d6e2ce1ef60
Hassan Ali <103069649+hassanalishah0335@users.noreply.github.com>
2024-11-09 19:27:29 +0500

    Update Experiance.js
    Experience

--- src/app/components/Experiance.js
+++ src/app/components/Experiance.js
@@ -3,14 +3,14 @@ import React from 'react'
 const Experiance = () => {
     return (
         <div className='my-20 text-white'>
-            <h1 className="  flex justify-center w-full text-4xl font-bold">Experiance</h1>
+            <h1 className="  flex justify-center w-full text-4xl font-bold">Experience</h1>
             <div className="flex flex-col items-start my-16 border-4  shadow-2xl hover:shadow-purple-500  rounded-3xl p-10">
                 <h1 className=' underline underline-offset-8 font-bold text-xl'>Software Developer</h1>
                 <p className=" text-gray-300">Einix Solutions Pvt.Ltd (July,2023 - ongoing)</p>
                 <p className="">Currently, I am working at Einix Solutions Pvt.Ltd Lahore as Fronted Developer with a proven track record of
                     engaging user interfaces and responsive web applications. Proficient in Python and JavaScript,
                     with hands-on experience using frameworks such as React and Next JS. Demonstrated ability to collaborate
-                    effectively with cross pafunctional teams to deliver high-quality projects on time.</p>
+                    effectively with cross functional teams to deliver high-quality projects on time.</p>
             </div>
         </div>
     )
