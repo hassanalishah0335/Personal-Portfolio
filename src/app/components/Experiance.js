6fa6aa823bce92139d77336060a68a9acd191583
Hassan Ali <103069649+hassanalishah0335@users.noreply.github.com>
2024-11-09 19:18:17 +0500

    Update Experiance.js
    experiance

--- src/app/components/Experiance.js
+++ src/app/components/Experiance.js
@@ -5,37 +5,12 @@ const Experiance = () => {
         <div className='my-20 text-white'>
             <h1 className="  flex justify-center w-full text-4xl font-bold">Experiance</h1>
             <div className="flex flex-col items-start my-16 border-4  shadow-2xl hover:shadow-purple-500  rounded-3xl p-10">
-                <h1 className=' underline underline-offset-8 font-bold text-xl'>Web Developer</h1>
-                <p className=" text-gray-300">Narsun (Feb-May,2024)</p>
-                <p className="">Currently, I am working at Narsun Studios Lahore as Fronted Developer with a proven track record of
-                    engaging user interfaces and responsive web applications. Proficient in Bootstrap, Tailwind, and JavaScript,
+                <h1 className=' underline underline-offset-8 font-bold text-xl'>Software Developer</h1>
+                <p className=" text-gray-300">Einix Solutions Pvt.Ltd (July,2023 - ongoing)</p>
+                <p className="">Currently, I am working at Einix Solutions Pvt.Ltd Lahore as Fronted Developer with a proven track record of
+                    engaging user interfaces and responsive web applications. Proficient in Python and JavaScript,
                     with hands-on experience using frameworks such as React and Next JS. Demonstrated ability to collaborate
                     effectively with cross pafunctional teams to deliver high-quality projects on time.</p>
-            </div>
-            <div className="flex flex-col items-start my-16 border-4  shadow-2xl hover:shadow-purple-500  rounded-3xl p-10">
-                <h1 className=' underline underline-offset-8 font-bold text-xl'>Mobile App Developer</h1>
-                <p className=" text-gray-300">BizzClan (Oct,2023-Jan,2024)</p>
-                <p className="">As a mobile application developer, My primary responsibility was to design, develop, and maintain mobile
-                    applications for various platforms such as iOS and Android. I had worked closely with other members of
-                    the development team, including designers and backend developers, to create user-friendly and visually
-                    appealing mobile apps that meet the needs and expectations of clients or end users. My tasks include
-                    writing code, debugging, testing, and optimizing the performance of mobile applications.</p>
-            </div>
-            <div className="flex flex-col items-start my-16 border-4  shadow-2xl hover:shadow-purple-500  rounded-3xl p-10">
-                <h1 className=' underline underline-offset-8 font-bold text-xl'>Frontend Intern</h1>
-                <p className=" text-gray-300">Bnody (Jun,2023-Aug,2023)</p>
-                <p className="">During my internship as a frontend developer specializing in the Tkinter library, I gained invaluable experience 
-                in creating intuitive and visually appealing user interfaces. Working closely with senior developers, I collaborated on projects 
-                aimed at enhancing user interaction and experience. Through hands-on experience with Tkinter, I developed expertise in designing 
-                and implementing graphical interfaces for desktop applications in Python.</p>
-            </div>
-            <div className="flex flex-col items-start my-16 border-4  shadow-2xl hover:shadow-purple-500  rounded-3xl p-10">
-                <h1 className=' underline underline-offset-8 font-bold text-xl'>Technical Support Executive</h1>
-                <p className=" text-gray-300">MindBridge (Dec,2022-May,2023)</p>
-                <p className="">During my tenure as a Technical Support Executive at Mindbridge Private Limited, I played a pivotal role in
-                    ensuring seamless customer experiences by providing efficient and effective technical support. Mindbridge,
-                    being a technology-focused company, presented a dynamic environment where my responsibilities ranged
-                    from troubleshooting technical issues to fostering positive client relationships.</p>
             </div>
         </div>
     )
