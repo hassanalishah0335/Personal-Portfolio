dc16628afa3141bce9b03bed3486e3878a96bcb7
Hassan Ali <103069649+hassanalishah0335@users.noreply.github.com>
2024-11-09 19:30:35 +0500

    Update Chat.js
    chat

--- src/app/components/Chat.js
+++ src/app/components/Chat.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Chat = () => {
   return (
-    <div className='fixed right-5 bottom-5 '>
+    <div className='fixed right-5 bottom-5 z-50'>
       <Link href="skype:live:hassanalishah75" target="_blank"><img className='w-16' src="/images/skype.png" alt="" /></Link>
       <Link href="https://wa.me/+923350183868" target="_blank"><img className='w-[70px] mt-2' src="/images/WhatsApp.png" alt="" /></Link>
     </div>
