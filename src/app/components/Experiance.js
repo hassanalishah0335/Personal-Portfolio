de2557ccec92fe1f71df8d374e97d7876887a26e
Hassan Ali <103069649+hassanalishah0335@users.noreply.github.com>
2024-12-11 23:36:44 +0500

    Update Experiance.js
    Inex

--- src/app/components/Experiance.js
+++ src/app/components/Experiance.js
@@ -3,14 +3,21 @@ import React from 'react'
 const Experiance = () => {
     return (
         <div className='my-20 text-white'>
-            <h1 className="  flex justify-center w-full text-4xl font-bold">Experience</h1>
+            <h1 className="  flex justify-center w-full text-4xl font-bold">Experiance</h1>
             <div className="flex flex-col items-start my-16 border-4  shadow-2xl hover:shadow-purple-500  rounded-3xl p-10">
                 <h1 className=' underline underline-offset-8 font-bold text-xl'>Software Developer</h1>
-                <p className=" text-gray-300">Einix Solutions Pvt.Ltd (July,2023 - ongoing)</p>
-                <p className="">Currently, I am working at Einix Solutions Pvt.Ltd Lahore as Fronted Developer with a proven track record of
-                    engaging user interfaces and responsive web applications. Proficient in Python and JavaScript,
-                    with hands-on experience using frameworks such as React and Next JS. Demonstrated ability to collaborate
-                    effectively with cross functional teams to deliver high-quality projects on time.</p>
+                <p className=" text-gray-300">Inex (May,2024-Currently) On-site</p>
+                <p className="">At Inex Solutions, I serve as a Software Developer, where I specialize in crafting dynamic and user-friendly applications. My work involves leveraging modern frameworks and libraries such as Next.js, Vue.js, and Material-UI to deliver scalable and visually appealing solutions. This role allows me to apply and expand my expertise in frontend development while contributing to innovative projects.</p>
+            </div>
+            <div className="flex flex-col items-start my-16 border-4  shadow-2xl hover:shadow-purple-500  rounded-3xl p-10">
+                <h1 className=' underline underline-offset-8 font-bold text-xl'>Web Developer</h1>
+                <p className=" text-gray-300">Sigi Technologies (May,2024-Oct,2024) Hybird</p>
+                <p className="">At Sigi Technologies, I worked as a remote developer, aligning my schedule with Sydney timings to ensure seamless collaboration. My role involved working with an international team, which provided valuable exposure to diverse perspectives and enhanced my ability to adapt to global work environments. This experience allowed me to contribute effectively to projects while maintaining excellent communication across different time zones.</p>
+            </div>
+            <div className="flex flex-col items-start my-16 border-4  shadow-2xl hover:shadow-purple-500  rounded-3xl p-10">
+                <h1 className=' underline underline-offset-8 font-bold text-xl'>Frontend Developer</h1>
+                <p className=" text-gray-300">Narsun Studio (Feb,2023-May,2024)</p>
+                <p className="">At Narsun Studios, I began my journey as an intern and was subsequently promoted to the role of Frontend Developer. During my time there, I honed my skills by working with various frameworks such as React, Bootstrap, and Tailwind CSS, contributing to diverse projects and enhancing user experiences through clean, responsive, and efficient design.</p>
             </div>
         </div>
     )
